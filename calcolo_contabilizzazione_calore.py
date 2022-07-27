5b8e011714654cbbe20c09e17014ef3e48ad8a2e
pdomi2001 <paride.dominici@gmail.com>
2022-07-27 18:08:37 +0200

    effettuate alcune ottimizzazioni

--- calcolo_contabilizzazione_calore.py
+++ calcolo_contabilizzazione_calore.py
@@ -1,130 +1,88 @@
+
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
 
 df = pd.read_csv('registrazioni_new.txt', sep="\t", header=0)
-
 fig, axs = plt.subplots(nrows = 3, ncols = 1)
+
 fig.set_facecolor('lightsteelblue')
 fig.tight_layout()
+
 # df_filtered = df[(df.clientID=='iot_paride_2') & (df.time=="2022-04-05 10:2")]
+giorni_precedenti = 2
+SECONDI_GIORNO = 60*60*24
+# ----------------------------------------
 
 giorno = 20220414
-giorni_precedenti = 2
+INIZIO_CONTEGGIO = 28.0 # gradi centigradi
+DELTA_T_CONTEGGIO = 120 # secondi
+GIORNOSINGOLO = False
+VERBOSE = False
+
+# ----------------------------------------
 
 # inizio_periodo = 20220407000000
 # fine_periodo = 20220408000000
-inizio_periodo = giorno * 1000000
-fine_periodo = (giorno + 1) * 1000000
 
-inizio_periodo_prec = (giorno - giorni_precedenti) * 1000000
+inizio_periodo = giorno * 1e6
+fine_periodo = inizio_periodo + 1e6
+# (giorno+1)*1e6 = giorno*1e6+1*1e6 = inizio_periodo+1e6
+inizio_periodo_prec = inizio_periodo - giorni_precedenti * 1e6
+# (giorno-giorni_precedenti)*1e6 = giorno*1e6-giorni_precedenti*1e6 = inizio_giorni - giorni_precedenti*1e6
 
-def converti_data_ora(x):
-	# ora = str(x.time % 1000000)
-	ora = ("%06d" % (x.time % 1000000))
-	# print (x.time)
-	# print (ora[0:2] + ":" + ora[2:4] + ":" + ora[4:6])
-	# return (ora[0:2] + ":" + ora[2:4] + ":" + ora[4:6])
-	return (int(x.time % 1000000) / 10000.0)
+def converti_data_ora(time):
+	return int(time%1e6) / 1e6
 
 #aggiungo un campo nel quale è presente solo l'orario
-df['time2']  = df.apply(converti_data_ora, axis = 1)
+df['time2']  = df["time"].apply(converti_data_ora)
 
 #aggiungo un campo nel quale è presente solo la data
-df['date']  = df.apply(lambda x: str(x.time / 1000000)[:8] , axis = 1)
 
-# print(df.head())
+df['date']  = df["time"].apply(lambda x: str(x / 1e6)[:8])
+df_filtered_1 = df[df.clientID=='iot_paride_1']
 
-df_filtered_1 = df[
-									(df.clientID=='iot_paride_1')
-								]
-# ----------------------------------------
-temp_inizio_conteggio = 28.0 # gradi centigradi
-delta_tempo_conteggio = 120 # secondi
-giornosingolo = False
-verbose = False
-# ----------------------------------------
+
 #filtro un giorno solo
-if (giornosingolo):
-	df_filtered_1_d = df_filtered_1[
-									 (df_filtered_1.time >= inizio_periodo)
-									 & (df_filtered_1.time <= fine_periodo)
-									 & (df_filtered_1.t_termosifone <= 60)
-								]
-else:					
-	df_filtered_1_d = df_filtered_1[
-									 (df_filtered_1.t_termosifone <= 60)
-								]
-df_filtered_1_t = df_filtered_1_d[
-									(df_filtered_1_d.t_termosifone >= temp_inizio_conteggio)
-								]
-
-# print (df_filtered_1_t)
-# print (df_filtered_2)
-# print (df_filtered_3)
-
+if GIORNOSINGOLO:
+	df_filtered_1_d = df_filtered_1[ df_filtered_1.time >= inizio_periodo
+								   & df_filtered_1.time <= fine_periodo
+								   & df_filtered_1.t_termosifone <= 60]
+else:
+	df_filtered_1_d = df_filtered_1[df_filtered_1.t_termosifone <= 60]
+df_filtered_1_t = df_filtered_1_d[df_filtered_1_d.t_termosifone >= INIZIO_CONTEGGIO]
 def ConvertiInSecondi(timestr):
-	# print ("Converto [%s]" % timestr)
-	sec = int(timestr[-2:])
-	min = int(timestr[-4:-2])
-	ora = int(timestr[-6:-4])
-	giorno = int(timestr[6:8])
-	mese = int(timestr[4:6])
-	anno = int(timestr[0:4])
-	# print (type(giorno), type(mese), type(anno))
-	secondi = sec + min * 60 + ora * 3600
-	giorniinizio = datetime(int(anno), int(mese), int(giorno)).toordinal()
-	secondi = secondi + 60 * 60 * 24 * giorniinizio 
-	# secondi = secondi + giorno * 3600 * 24
-	# secondi = secondi + mese * 3600 * 24 * 31
-	# secondi = secondi + anno * 3600 * 24 * 31 * 12
-	# print(secondi)
-	return secondi
-
+	sec,min_,ora = map(int,(timestr[-2:],timestr[-4:-2],timestr[-6:-4]))
+	giorno,mese,anno = map(int,(timestr[6:8],timestr[4:6],timestr[:4]))
+	secondi = sec + min_ * 60 + ora * 3600
+	giorniinizio = datetime(anno, mese, giorno).toordinal()
+	return secondi + SECONDI_GIORNO * giorniinizio
 elem = df_filtered_1_t['time'].values.tolist()[0]
 # print (df_filtered_1_t[(df_filtered_1_t['time'] == elem)].values.tolist()[0])
 lastcount = 0
 contabilizzatore = 0
-for elem in df_filtered_1_t['time'].values.tolist():
-	time, ip, str_sensore, h_stanza, t_stanza, t_termosifone, count_sent, count_wifi_c, count_mqtt_c, time2, data \
-		= df_filtered_1_t[(df_filtered_1_t['time'] == elem)].values.tolist()[0]
-	# print(e['count_sent'])
-	# print(lastcount)
+for (time, ip, str_sensore, h_stanza, t_stanza, t_termosifone,
+	 count_sent, count_wifi_c, count_mqtt_c, time2, data) in df_filtered_1_t.values.tolist():
 	time = str(time)
-	if lastcount != 0:
-		# if elem == lastelem + 1:
-		# if int(e['count_sent']) == lastcount + 1:
+	if lastcount:
+	
 		if int(count_sent) == lastcount + 1:
-			# contabilizzatore = contabilizzatore + 1
 			endtime = time
-			# endtime = e['time'].astype("string")
-			# print("tipo endtime", type(endtime))
 		else:
+		
 			secondi = ConvertiInSecondi(endtime) - ConvertiInSecondi(starttime)
-			if (verbose):
-				print ("conteggio da %s a %s = %d" % (starttime, endtime, secondi))
-			unita_contabilizzate = int(secondi / delta_tempo_conteggio)
-			if (verbose):
-				print ("unita contabilizzate %d / %d = %d" % (secondi, delta_tempo_conteggio, unita_contabilizzate))
-			contabilizzatore = contabilizzatore + unita_contabilizzate 
-			if (verbose):
+			if (VERBOSE):
+				print ("conteggio da %s a %s = %d secondi" % (starttime, endtime, secondi))
+			unita_contabilizzate = int(secondi / DELTA_T_CONTEGGIO)
+			if (VERBOSE):
+				print ("unita contabilizzate %d / %d = %d" % (secondi, DELTA_T_CONTEGGIO, unita_contabilizzate))
+			contabilizzatore +=  unita_contabilizzate 
+			if (VERBOSE):
 				print ("nuovo conteggio", contabilizzatore)
 			starttime = time
-			# starttime = e['time'].to_string()
-			# print("tipo starttime", type(starttime))
 	else:
 		starttime = time
-		# starttime = e['time'].to_string()
-		# print("tipo starttime", type(starttime))
 	lastcount = count_sent
-	# lastcount = pd.to_numeric(e['count_sent'])
-	# print("tipo lastcount", type(lastcount))
-	# print(lastcount)
-	# print(e['count_sent'])
-	# print(e)
-	# print('----')
-	# print (elem, count, df_filtered_1_t[(df_filtered_1_t['count_sent'] == elem)])
-	# print (df_filtered_1_t[(df_filtered_1_t['time'] == elem)])
 
 print("Unita' contabilizzate: %d" % contabilizzatore)
 
